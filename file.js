735701580991e5405a8758dcb823c074a03c402d
Vano <email.com>
2023-09-30 17:25:53 +0300

    The second commit

--- file.js
+++ file.js
@@ -1,28 +1,27 @@
 const fs = require('fs-extra')
 
-const dir1 = 'C:/Users/van0t/JS-automatization/Homework_2/New_folder_1'
+const dir1 = 'Homework_2/New_folder_1'
 fs.ensureDirSync(dir1)
 
-const file = 'C:/Users/van0t/JS-automatization/Homework_2/New_folder_1/file.txt'
+const file = 'Homework_2/New_folder_1/file.txt'
 fs.ensureFileSync(file)
 
-const dir2 = 'C:/Users/van0t/JS-automatization/Homework_2/New_folder_2'
+const dir2 = 'Homework_2/New_folder_2'
 fs.ensureDirSync(dir2)
 
-fs.moveSync('C:/Users/van0t/JS-automatization/Homework_2/New_folder_1/file.txt', 
-'C:/Users/van0t/JS-automatization/Homework_2/New_folder_2/file.txt', { overwrite: true})
+fs.moveSync('Homework_2/New_folder_1/file.txt', 'Homework_2/New_folder_2/file.txt', {overwrite: true})
 
-const dir3 = 'C:/Users/van0t/JS-automatization/Homework_2/New_folder_3'
+const dir3 = 'Homework_2/New_folder_3'
 fs.ensureDirSync(dir3)
 
-fs.moveSync('C:/Users/van0t/JS-automatization/Homework_2/New_folder_2/file.txt', 
-'C:/Users/van0t/JS-automatization/Homework_2/New_folder_3/file.txt', { overwrite: true})
+fs.copySync('Homework_2/New_folder_2/file.txt', 'Homework_2/New_folder_3/file.txt', {overwrite: true})
 
-fs.removeSync('C:/Users/van0t/JS-automatization/Homework_2/New_folder_3/file.txt')
+fs.removeSync('Homework_2/New_folder_2/file.txt')
+fs.removeSync('Homework_2/New_folder_3/file.txt')
 
-fs.removeSync('C:/Users/van0t/JS-automatization/Homework_2/New_folder_1') 
-fs.removeSync('C:/Users/van0t/JS-automatization/Homework_2/New_folder_2') 
-fs.removeSync('C:/Users/van0t/JS-automatization/Homework_2/New_folder_3')
+fs.removeSync('Homework_2/New_folder_1') 
+fs.removeSync('Homework_2/New_folder_2') 
+fs.removeSync('Homework_2/New_folder_3')
 
 
 
