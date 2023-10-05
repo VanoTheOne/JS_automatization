243ad8c601a90cfdc1626c5a3940758412998423
Vano <email.com>
2023-10-05 12:24:30 +0300

    The third homework

---
+++ Homework_3/operations.js
@@ -0,0 +1,64 @@
+//1.1
+let str1 = 'Hello';
+let someThing1 = true;
+console.log(str1+someThing1);
+//1.2
+let str2 = 'Hello';
+let someNum1 = 21;
+console.log(str2+someNum1);
+//1.3
+let someNum2 = 53;
+let someThing2 = false;
+console.log(someNum2+someThing2);
+
+//2.1
+let str3 = 'Hello';
+let someThing3 = true;
+console.log(str3*someThing3);
+//2.2
+let str4 = 'Hello';
+let someNum3 = 21;
+console.log(str4*someNum3);
+//2.3
+let someNum4 = 53;
+let someThing4 = false;
+console.log(someNum4*someThing4);
+
+//3.1
+let str5 = 'Hello';
+let someThing5 = true;
+console.log(str5/someThing5);
+//3.2
+let str6 = 'Hello';
+let someNum5 = 21;
+console.log(str6/someNum5);
+//3.3
+let someNum6 = 53;
+let someThing6 = false;
+console.log(someNum6/someThing6);
+
+//4.1
+const value1 = '9';
+const value2 = 77;
+
+let sum1 = Number(value1) + value2;
+
+console.log(sum1);
+console.log(typeof sum1);
+//4.2
+const value3 = 9;
+const value4 = 77;
+
+let sum2 = value3 + String(value4);
+
+console.log(sum2);
+console.log(typeof sum2);
+
+//4.3
+const value5 = 9;
+const value6 = '77';
+
+let sum3 = Boolean(value5) + Boolean(value6);
+
+console.log(sum3);
+console.log(typeof sum3);
