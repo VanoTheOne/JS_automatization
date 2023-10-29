ecab4132a7397158a645b9a42fc7082ebe98be83
Vano <email.com>
2023-10-29 18:49:56 +0300

    Little fix

--- Homework_6/PasteInArray.js
+++ Homework_6/PasteInArray.js
@@ -10,11 +10,13 @@ let resultArr = pasteInArr();
 console.log("arr:", resultArr);
 
 //5
+let arr1 = [1, 2, 3, 4, 5];
+
 function pasteInArr1() {
-    arr.splice(1, 0, "a", "b");
-    arr.splice(6, 0, "c");
-    arr.splice(8, 0, "e");
-    return arr;
+    arr1.splice(1, 0, "a", "b");
+    arr1.splice(6, 0, "c");
+    arr1.splice(8, 0, "e");
+    return arr1;
 }
 
 let resultArr1 = pasteInArr1();
--- Homework_6/SortArray.js
+++ Homework_6/SortArray.js
@@ -1,9 +1,10 @@
 //6
-//just function
+
 let numArray = [3, 4, 1, 2, 7, 30, 50];
 
-numArray.sort(function (a, b) {
-  return b - a;
-});
+function sortArray(a, b) {
+  return (b - a);
+  }
 
-console.log(numArray);
+let resultArray = numArray.slice().sort(sortArray);
+console.log(`Sorted array: `, resultArray);
