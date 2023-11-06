d6c301bacbfd415b1e06f3036437cb8b62ab6131
Vano <email.com>
2023-11-06 20:46:33 +0300

    The Type Script homework

---
+++ Homework_9/Hometasks/Task 2/Person.ts
@@ -0,0 +1,37 @@
+"use strict"
+
+import { User } from "../Task 1/User";
+
+interface Admin {
+    name: string,
+    age: number,
+    role?: string
+}
+
+interface Person extends User, Admin {};
+
+const persons: Person[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    }
+];
+
+console.log(persons);
+
